d325c592201322ba1a9a949ecd37f4273e66cc85
Marcus Goncalves <marcus.goncalves1@walmart.com>
2017-12-19 17:42:00 -0200

    Layout page and components Product

--- src/components/product-item/product-item.jsx
+++ src/components/product-item/product-item.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 function ProductItem({ title, picture, price }) {
   return (
-    <li className="product">
-      <figure className="product-figure">
-        <img src={picture} className="product-img" alt={title} />
+    <li className="product-item">
+      <figure className="product-item-figure">
+        <img src={picture} className="product-item-img" alt={title} />
       </figure>
       <div className="product-info">
         <h2 className="product-title">{title}</h2>
--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -28,31 +28,30 @@ class Product extends PureComponent {
 
   render() {
     return (
-      <div className="product wrapper">
-        { this.state.item &&
-          <div className="product-wrapper-main">
-            <section className="product-main">
-              <figure className="product-figure">
-                <img src={this.state.item.picture} alt={this.state.item.title} />
-              </figure>
-              <div className="product-info-area">
-                <h1 className="product-title">{this.state.item.title}</h1>
-                <p className="product-price">$ {this.state.item.price.amount}</p>
-                <div className="product-details">
+      <section className="main">
+        <div className="wrapper">
+          { this.state.item &&
+            <div className="product-wrapper-main">
+              <section className="product-main">
+                <figure className="product-figure">
+                <img src={this.state.item.picture} className="product-img" alt={this.state.item.title} />
+                </figure>
+                <div className="product-info-area">
+                  <h1 className="product-title">{this.state.item.title}</h1>
+                  <p className="product-price">$ {this.state.item.price.amount}</p>
+                  <div className="product-buy">
+                    <button className="product-btn-buy">Comprar</button>
+                  </div>
                 </div>
-                <div className="product-buy">
-                  <button>Comprar</button>
-                </div>
-              </div>
-            </section>
-
-            <section className="product-description">
-              <h2 className="title">Descrição do produto</h2>
-              <p className="description">{this.state.item.description}</p>
-            </section>
-          </div>
-        }
-      </div>
+              </section>
+              <section className="product-description">
+                <h2 className="title">Descrição do produto</h2>
+                <p className="description">{this.state.item.description}</p>
+              </section>
+            </div>
+          }
+        </div>
+      </section>
     );
   }
 }
